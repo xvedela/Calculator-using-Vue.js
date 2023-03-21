db091eeef58cc7c14dae252f384aa593174ed9c9
dato.xvedelidze.888@gmail.com <116672666+xvedela@users.noreply.github.com>
2023-03-21 20:58:13 +0400

    add javascript

--- src/components/views/Calculator.vue
+++ src/components/views/Calculator.vue
@@ -1,43 +1,77 @@
+
+<template>
+    <div class="calculator">
+        <div class="display">
+            {{ current || 0 }}
+        </div>
+        <div @click="clear" class="btn">C</div>
+        <div @click="sign()" class="btn operator">+/-</div>
+        <div @click="persent('%')" class="btn operator">%</div>
+        <div @click="append('/')" class="btn operator">/</div>
+        <div @click="append('7')" class="btn">7</div>
+        <div @click="append('8')" class="btn">8</div>
+        <div @click="append('9')" class="btn">9</div>
+        <div @click="append('*')" class="btn operator">*</div>
+        <div @click="append('4')" class="btn">4</div>
+        <div @click="append('5')" class="btn">5</div>
+        <div @click="append('6')" class="btn">6</div>
+        <div @click="append('-')" class="btn operator">-</div>
+        <div @click="append('1')" class="btn">1</div>
+        <div @click="append('2')" class="btn">2</div>
+        <div @click="append('3')" class="btn">3</div>
+        <div @click="append('+')" class="btn operator">+</div>
+        <div @click="append('0')" class="btn zero">0</div>
+        <div @click="dot()" class="btn">.</div>
+        <div @click="equal()" class="btn operator">=</div>
+        <div @click="" class="btn">History</div>
+    </div>
+</template>
+
+
+
+
+
 <script>
 
-
 export default {
-    name: 'Calcilator'
+    name: 'Calcilator',
+    data() {
+        return {
+            current: '',
+            operator: null,
+
+
+        }
+    },
+    methods: {
+        clear() {
+            this.current = ''
+        },
+        sign(){
+            this.current = this.current.charAt(0) === '-' ?
+            this.current.slice(1) : '-${this.current}'
+        },
+        persent(){
+            this.current = '${parseFloat(this.current)/100}'
+        },
+        append(number){
+            this.current = '${this.current}${number}'
+        },
+        dot(){
+            if(this.current.indexOf('.')===-1){
+                this.append('.')
+            }
+        },
+        equal(){
+            this.current = eval(this.current)
+        }
+    }
 
 
 
 }
 
 </script>
-
-
-<template>
-<div class="calculator">
-    <div class="display">
-        {{ current || 0}}
-    </div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn">*</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn">+</div>
-    <div class="btn">0</div>
-    <div class="btn">.</div>
-    <div class="btn">=</div>
-</div>
-
-</template>
 
 
 <style>
@@ -50,11 +84,24 @@ export default {
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(50px, auto);
 }
+
 .display {
     grid-column: 1/5;
     background-color: #333;
     color: white;
 }
 
+.btn {
+    background-color: #f2f2f2;
+    border: 1px solid #999;
+}
 
+.zero {
+    grid-column: 1/3;
+}
+
+.operator {
+    background-color: orange;
+    color: white;
+}
 </style>
