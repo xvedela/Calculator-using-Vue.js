7b81d847fa36c8b5556a5160f687bce1eba74359
dato.xvedelidze.888@gmail.com <116672666+xvedela@users.noreply.github.com>
2023-03-21 20:11:46 +0400

    created Calculator.vue and added numbers for calculator

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
-<script setup>
+<script>
+import Calculator from "./components/views/Calculator.vue"
 
+export default {
+    name: "app",
+    components: {
+        Calculator
+    }
+};
 </script>
 
 <template>
-
+    <div id="app">
+        <Calculator />
+    </div>
 </template>
+
+<style>
+#app {
+    font-family: "Avenir", Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 30px;
+    color: #2c3e50;
+}
+</style>
 
---
+++ src/components/views/Calculator.vue
@@ -0,0 +1,60 @@
+<script>
+
+
+export default {
+    name: 'Calcilator'
+
+
+
+}
+
+</script>
+
+
+<template>
+<div class="calculator">
+    <div class="display">
+        {{ current || 0}}
+    </div>
+    <div class="btn">C</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">*</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">+</div>
+    <div class="zero">0</div>
+    <div> class="btn".</div>
+    <div class="btn">=</div>
+</div>
+
+</template>
+
+
+<style>
+.calculator {
+    cursor: pointer;
+    width: 400px;
+    font-size: 40px;
+    display: grid;
+    margin: 0 auto;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+}
+.display {
+    grid-column: 1/5;
+    background-color: #333;
+    color: white;
+}
+
+
+</style>
