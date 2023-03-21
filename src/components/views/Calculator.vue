cce4915b138bb0fe087449bef6cfdd0c180f2d60
dato.xvedelidze.888@gmail.com <116672666+xvedela@users.noreply.github.com>
2023-03-21 21:12:28 +0400

    calculator working

--- src/components/views/Calculator.vue
+++ src/components/views/Calculator.vue
@@ -5,8 +5,8 @@
             {{ current || 0 }}
         </div>
         <div @click="clear" class="btn">C</div>
-        <div @click="sign()" class="btn operator">+/-</div>
-        <div @click="persent('%')" class="btn operator">%</div>
+        <div @click="sign" class="btn operator">+/-</div>
+        <div @click="percent" class="btn operator">%</div>
         <div @click="append('/')" class="btn operator">/</div>
         <div @click="append('7')" class="btn">7</div>
         <div @click="append('8')" class="btn">8</div>
@@ -21,56 +21,45 @@
         <div @click="append('3')" class="btn">3</div>
         <div @click="append('+')" class="btn operator">+</div>
         <div @click="append('0')" class="btn zero">0</div>
-        <div @click="dot()" class="btn">.</div>
-        <div @click="equal()" class="btn operator">=</div>
-        <div @click="" class="btn">History</div>
+        <div @click="dot" class="btn">.</div>
+        <div @click="equal" class="btn operator">=</div>
+        <div class="btn">History</div>
     </div>
 </template>
 
-
-
-
-
 <script>
-
 export default {
-    name: 'Calcilator',
+    name: 'Calculator',
     data() {
         return {
             current: '',
             operator: null,
-
-
-        }
+        };
     },
     methods: {
         clear() {
-            this.current = ''
+            this.current = '';
         },
-        sign(){
+        sign() {
             this.current = this.current.charAt(0) === '-' ?
-            this.current.slice(1) : '-${this.current}'
+            this.current.slice(1) : `-${this.current}`;
         },
-        persent(){
-            this.current = '${parseFloat(this.current)/100}'
+        percent() {
+            this.current = `${parseFloat(this.current) / 100}`;
         },
-        append(number){
-            this.current = '${this.current}${number}'
+        append(number) {
+            this.current = `${this.current}${number}`;
         },
-        dot(){
-            if(this.current.indexOf('.')===-1){
-                this.append('.')
+        dot() {
+            if (this.current.indexOf('.') === -1) {
+                this.append('.');
             }
         },
-        equal(){
-            this.current = eval(this.current)
-        }
-    }
-
-
-
-}
-
+        equal() {
+            this.current = eval(this.current);
+        },
+    },
+};
 </script>
 
 
